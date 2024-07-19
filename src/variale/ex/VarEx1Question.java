f9790c5f71521b7cec4e7525488f34e42991b3ee
KIMYOUNGGON1 <dudrhs950@naver.com>
2024-07-19 22:48:45 +0900

    변수 수업 내용

---
+++ src/variale/ex/VarEx1Question.java
@@ -0,0 +1,11 @@
+package variale.ex;
+
+public class VarEx1Question {
+    public static void main(String[] args) {
+        int firstNum = 4;
+        int secondNum = 3;
+        System.out.println(firstNum + secondNum);
+        System.out.println(firstNum - secondNum);
+        System.out.println(firstNum * secondNum);
+    }
+}
---
+++ src/variale/ex/VarEx3Question.java
@@ -0,0 +1,11 @@
+package variale.ex;
+
+public class VarEx3Question {
+    public static void main(String[] args) {
+        long testVar = 10000000000L;
+        System.out.println("testVar = " + testVar);
+
+        boolean testVar2 = true;
+        System.out.println("testVar2 = " + testVar2);
+    }
+}
