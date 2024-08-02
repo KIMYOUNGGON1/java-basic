8afde5825eb9e7069d34f8b861316184a181ac2d
KIMYOUNGGON1 <dudrhs950@naver.com>
2024-08-02 09:18:28 +0900

    배열 문제 풀이

--- src/array/ex/ArrayEx8.java
+++ src/array/ex/ArrayEx8.java
@@ -2,13 +2,16 @@ package array.ex;
 
 import java.util.Scanner;
 
-public class ArrayEx7 {
+public class ArrayEx8 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[][] studentsGrade = new int[4][3];
+        System.out.print("학생수를 입력하세요:");
+        int studentCount = input.nextInt();
+
+        int[][] studentsGrade = new int[studentCount][3];
         String[] subjects = {"국어", "영어", "수학"};
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < studentCount; i++) {
             System.out.println((i + 1) + "번 학생의 성적을 입력하세요:");
             for (int j = 0; j < 3; j++) {
                 System.out.print(subjects[j] + " 점수:");
@@ -16,7 +19,7 @@ public class ArrayEx7 {
             }
         }
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < studentCount; i++) {
             int total = 0;
             for (int j = 0; j < 3; j++) {
                 total += studentsGrade[i][j];
